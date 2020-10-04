76a09697869bda8e6da18d7a46c40ff5d87be3c1
samundra <samundranepal001@gmail.com>
2020-10-04 12:49:41 +0545

    host

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
-const socket = io.connect('/')
+const socket = io.connect('/room/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
-  host: '/',
+  host: '/room/',
   port: '443'
 })
 let myVideoStream;
